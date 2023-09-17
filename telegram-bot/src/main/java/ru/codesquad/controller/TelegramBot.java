c8c43bb5d786ad71c01da3ec69fb415d51f1ef5f
Denis <leonenko.denis.1@yandex.ru>
2023-09-17 22:46:53 +0300

    00.telegram-bot/01.03 - feat: added start message

--- telegram-bot/src/main/java/ru/codesquad/controller/TelegramBot.java
+++ telegram-bot/src/main/java/ru/codesquad/controller/TelegramBot.java
@@ -4,7 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import static ru.codesquad.util.ResponseTemplates.START_MESSAGE_1;
+import static ru.codesquad.util.ResponseTemplates.START_MESSAGE_2;
 
 //TODO В данный момент бот написан с технологией LongPolling, в дальнейшем переделать на Webhooks
 
@@ -31,5 +36,27 @@ public class TelegramBot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         var message = update.getMessage();
         log.info(message.getText());
+
+        var response = new SendMessage();
+        response.setChatId(message.getChatId().toString());
+        response.setText(START_MESSAGE_1);
+        sendAnswerMessage(response);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        response.setText(START_MESSAGE_2);
+        sendAnswerMessage(response);
+    }
+
+    public void sendAnswerMessage(SendMessage message) {
+        if (message != null) {
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                log.error(e.getMessage());
+            }
+        }
     }
 }
---
+++ telegram-bot/src/main/java/ru/codesquad/util/ResponseTemplates.java
@@ -0,0 +1,8 @@
+package ru.codesquad.util;
+
+public class ResponseTemplates {
+    public static final String START_MESSAGE_1 = "Привет!";
+    public static final String START_MESSAGE_2 = "Это бот создан для помощи заводчикам в ведении своего бизнеса," +
+            " а также для людей, ищущих своего нового питомца.";
+
+}
